ea039c0381e3a252f98d103fd6493c8376d16643
tazxs <doni.nurbaev@gmail.com>
2024-10-29 23:31:48 +0500

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import './index.scss'
 function App() {
+  const [count,setCount] = useState(0);
+  const onClickPlus = () =>{
+    setCount(count + 1);
+  }
+  const onClickMinus = () =>{
+    setCount(count - 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Счетчик:</h2>
+      <h1 className="counts">{count}</h1>
+      <button className="minus" onClick={onClickMinus}>- Минус</button>
+      <button className="plus" onClick={onClickPlus}>Плюс +</button>
     </div>
+  </div>
   );
 }
 
